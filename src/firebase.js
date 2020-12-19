6d566b12c6454435c240ae9799c760e14636c960
ChristianAven <chrisavendano0508@gmail.com>
2020-12-19 02:03:01 -0500

    creacion de la carpeta de los componentes

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAJnjkoNzdNS_I6lfDSnnO3XMGwCiKdsgg",
+    authDomain: "facebook-clone-958ba.firebaseapp.com",
+    projectId: "facebook-clone-958ba",
+    storageBucket: "facebook-clone-958ba.appspot.com",
+    messagingSenderId: "297632662777",
+    appId: "1:297632662777:web:6736428a7c662ef32c4e28",
+    measurementId: "G-W4KR74ZSCH"
+  };
